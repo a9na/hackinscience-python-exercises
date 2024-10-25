81428f68e27b745609fec97dba7bbf3ab487b650
Ana Novković <anaiscoding@gmail.com>
2024-10-25 14:39:54 +0200

    Create print-42.py

---
+++ print-42.py
@@ -0,0 +1,10 @@
+# In this exercise you'll learn to use a function by trying the print builtin function.
+# A function is a named thing that does something specific:
+# the print function displays what's given to it,
+# the len function measures the thing we give to it,
+# …
+# The syntax to make a function work is called a "function call", to make print display the string "Hello world" the syntax is:
+# print("Hello world")
+
+number=42
+print(number)
